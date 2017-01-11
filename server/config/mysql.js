fb18f2351eda99b95e5fa6fbec59c3c1f6c26111
Chris Rollins <chris.rollins@leapfire.com>
2017-01-10 19:30:57 -0800

    some changes to templates, and a little registration

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -6,7 +6,8 @@ var connection = mysql.createConnection({
     host     : "localhost",
     user     : "root",
     password : "root",
-    database : "YPCdb"
+    // database : "YPCdb"
+    database: "mydb"
 });
 
 module.exports = connection;
