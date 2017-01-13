53a774fb89ef9bb1184cd3599e7c499606f3ee1d
Chris Rollins <chris.rollins@leapfire.com>
2017-01-12 18:15:23 -0800

    doQuery now uses a callback, updated templates to match

--- server/models/registrationModel.js
+++ server/models/registrationModel.js
@@ -1,7 +1,5 @@
 //template for a model file
 
-//require these two things to be able to do queries
-var connection = require('../config/mysql.js');
 var doQuery = require('../config/doquery_function.js');
 //the doQuery function is available. it takes a string which is the query, and an optional callback function
 //the callback function takes one argument, rows. it is an array returned from a successful query.
@@ -12,11 +10,15 @@ var doQuery = require('../config/doquery_function.js');
 var bcrypt = require("bcryptjs");
 
 module.exports = {
-	newUser: function(req, res){
-		console.log(req.body);
-		var hashedPW = bcrypt.hashSync(req.body.pw1);
-		var result = doQuery("insert into users (first_name, last_name, password, email, phone, created_at, updated_at) values ('"+ req.body.first_name + "', '" + req.body.last_name + "', '" + hashedPW + "', '" + req.body.email + "', '" + req.body.phone +", now(), now()')");
-		return result;
+	newUser: function(req, res, callback){
+		console.log(req.body.pw1);
+		var salt = bcrypt.genSaltSync(10);
+		var hashedPW = bcrypt.hashSync(req.body.pw1, salt);
+		doQuery("insert into users (first_name, last_name, password, email, created_at, updated_at) values ('"+ req.body.first_name + "', '" + req.body.last_name + "', '" + hashedPW + "', '" + req.body.email +"', now(), now())", callback);
+	},
+
+	checkEmail: function(req, res, callback){
+		doQuery("select id from users where users.email = '" + req.body.email + "'", callback);
 	}
 }
 
